2e3e6a425e77b35e9bd23893f9384a9e7e5cbdde
Man Navlakha <mannavlakha1021@gmail.com>
2024-12-13 20:27:05 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace @lock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnContactPS = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(387, 362);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(2);
+            this.label1.Size = new System.Drawing.Size(641, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "I am sorry for that your payment is pending. You need to first clear your payment" +
+    " then you can start your work.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(628, 328);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hello , this call from PS";
+            // 
+            // btnContactPS
+            // 
+            this.btnContactPS.Font = new System.Drawing.Font("Cascadia Code SemiBold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContactPS.Location = new System.Drawing.Point(660, 406);
+            this.btnContactPS.Margin = new System.Windows.Forms.Padding(0);
+            this.btnContactPS.Name = "btnContactPS";
+            this.btnContactPS.Size = new System.Drawing.Size(75, 23);
+            this.btnContactPS.TabIndex = 2;
+            this.btnContactPS.Text = "Contect us ";
+            this.btnContactPS.UseVisualStyleBackColor = true;
+            this.btnContactPS.Click += new System.EventHandler(this.btnContactPS_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(571, 673);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(255, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "All the right and legal copyright BY MAN NAVLAKHA";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(497, 113);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(435, 176);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(-1352, 293);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Olive;
+            this.ClientSize = new System.Drawing.Size(1298, 695);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnContactPS);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PS PC CARE";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnContactPS;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace @lock
+{
+    public partial class Form1 : Form
+    {
+        
+        public async Task<bool> CheckApiResponse()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetAsync("http://localhost/api.php?id=user123");
+
+                    // Ensure the response is successful
+                    response.EnsureSuccessStatusCode();
+
+                    // Read the response content
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    // Attempt to deserialize the content
+                    try
+                    {
+                        var lockStatus = JsonConvert.DeserializeObject<LockStatus>(content);
+
+                        // Show the lock status in a formatted message
+                        string statusMessage = lockStatus.is_locked == "1" ? "The system is currently LOCKED." : "The system is currently UNLOCKED.";
+                        MessageBox.Show($"Lock Status: {statusMessage}", "Lock Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        // Return true if locked, false if unlocked
+                        return lockStatus.is_locked == "1"; // Assuming "1" means locked
+                    }
+                    catch (JsonException)
+                    {
+                        // Handle the case where the content is not valid JSON
+                        MessageBox.Show("Received non-JSON response.");
+                        return false; // Default to unlocked on error
+                    }
+                }
+                catch (HttpRequestException httpEx)
+                {
+                    // Handle HTTP request errors
+                    MessageBox.Show($"HTTP error: {httpEx.Message}");
+                    return false; // Default to unlocked on error
+                }
+                catch (Exception ex)
+                {
+                    // Handle any other exceptions
+                    MessageBox.Show($"Error checking lock status: {ex.Message}");
+                    return false; // Default to unlocked on error
+                }
+            }
+        }
+        public class LockStatus
+        {
+            public string is_locked { get; set; } // This property matches the API response
+        }
+
+        // Event handler for the form load event
+        private async void MainForm_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show("MainForm_Load is called."); // Debugging line
+            bool isLocked = await CheckApiResponse();
+            if (isLocked)
+            {
+                this.Show(); // Show the lock screen if the computer is locked
+            }
+            else
+            {
+                // If payment is done, inform the user and close the application
+                MessageBox.Show("Payment has been cleared. The application will now close.");
+                Application.Exit(); // This will close the application
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            //MessageBox.Show("CheckLockStatus method is called.");
+            this.Load += new System.EventHandler(this.MainForm_Load);
+        }
+
+        private void btnContactPS_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
